1affd6c995c6af8f756e46805837e24a63192db1
roysth <roysoonth@gmail.com>
2022-07-26 22:26:40 +0800

    Edited Workshop 14

---
+++ src/main/java/sg/edu/nus/iss/workshop14/Controller/AddressBookController.java
@@ -0,0 +1,47 @@
+package sg.edu.nus.iss.workshop14.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import sg.edu.nus.iss.workshop14.Model.Contact;
+import sg.edu.nus.iss.workshop14.Service.ContactsRedis;
+
+@Controller
+public class AddressBookController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AddressBookController.class);
+
+    @Autowired
+    ContactsRedis service;
+
+    //http://localhost:8080/
+    @GetMapping("/")
+    public String contactForm(Model model) {
+        model.addAttribute("contact", new Contact());
+        return "contact";
+    }
+
+    //http://localhost:8080/contact/d7b626d8
+    @GetMapping("/contact/{contactId}")
+    public String getContact(Model model, @PathVariable(value = "contactId") String contactId) {
+        Contact cc = service.findById(contactId);
+        model.addAttribute("contact", cc);
+        return "showContact";
+    }
+
+    //http://localhost:8080/contact/
+    @PostMapping("/contact")
+    public String submitContact(@ModelAttribute Contact contact, Model model) {
+        logger.info("contact id > " + contact.getId());
+        service.save(contact);
+        model.addAttribute("contact", contact);
+        return "showContact";
+    }
+}
---
+++ src/main/java/sg/edu/nus/iss/workshop14/Service/ContactsRedis.java
@@ -0,0 +1,34 @@
+package sg.edu.nus.iss.workshop14.Service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import sg.edu.nus.iss.workshop14.Model.Contact;
+
+
+@Service
+public class ContactsRedis implements ContactsRepo {
+    private static final Logger logger = LoggerFactory.getLogger(ContactsRedis.class);
+
+    //Central class to interact with Redis data. To serialize object to stream, and deserealize stream to object
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+
+    //to override ContactsRepo?
+    @Override
+    public void save(final Contact ctc) {
+        //To access value operations (ID number)
+        redisTemplate.opsForValue().set(ctc.getId(), ctc);
+    }
+
+    @Override
+    public Contact findById(final String contactId) {
+        Contact result = (Contact) redisTemplate.opsForValue().get(contactId);
+        logger.info(">>> " + result.getEmail());
+        return result;
+    }
+}
